5dcc548661d12c85b5c589cfe8f98a75a19122a7
nikita <zerganrus@gmail.com>
2021-06-14 22:51:18 +0300

    refact: adds comment to IMessage type and add two "expect" statements to publisher test suite

--- test/publisher.spec.ts
+++ test/publisher.spec.ts
@@ -2,6 +2,7 @@ import {ServiceBroker} from "moleculer";
 import PublisherService from "../services/publisher.service";
 import * as Moleculer from "moleculer";
 import ValidationError = Moleculer.Errors.ValidationError;
+import {IMessage} from "../types/message.type";
 
 describe("Test publisher service", () => {
 	let broker = new ServiceBroker({logger: false})
@@ -13,7 +14,9 @@ describe("Test publisher service", () => {
 	describe("Test publisher.initMessage action for input type match", () => {
 		it("should return true with {'message': 'Hello', 'user': 'user1', timestamp: '1515125125'}", async() => {
 			try {
-				const res = await broker.call('publisher.initMessage', {message: 'Hello', user: 'user1', timestamp: 1515125125});
+				const input = {message: 'Hello', user: 'user1', timestamp: 1515125125}
+				const res = await broker.call('publisher.initMessage', input);
+				expect(input).toBeInstanceOf(IMessage);
 				expect(res).toBeTruthy();
 			} catch (e) {
 				console.log("It should not happen");
@@ -22,8 +25,9 @@ describe("Test publisher service", () => {
 
 		it("should throw error if input has mismatched type", async () => {
 			try {
-			 	await broker.call("publisher.initMessage", {random: 'object'}).catch(e => {
-			});
+				const input = { random: "object" };
+				expect(input).not.toBeInstanceOf(IMessage);
+			 	await broker.call("publisher.initMessage", input)
 			} catch (e) {
 				expect(e).toBeInstanceOf(ValidationError);
 			}
--- types/message.type.ts
+++ types/message.type.ts
@@ -1,3 +1,5 @@
+// I use class here to make it possible to faster check it in jest with toBeInstanceOf, otherwise
+// it definitely should be interface or type, of course
 export class IMessage {
 	user: string;
 	message: string;
